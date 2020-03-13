2883b2863f989b749228fa12fc01bf05838a4e35
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2020-03-13 19:45:13 -0300

    Exercise 1
    Not fully sure if I making right the EventEmmiter should check it out with the tutor later

---
+++ 02-oop-inheritance/Exercise 1/Actor.js
@@ -0,0 +1,23 @@
+class Actor{
+
+  constructor (name, age){
+    this.name = name;
+    this.age = age;
+  }
+
+  get age() {
+    return this.age;
+  }
+
+  set age(age) {
+    this.age = age;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(name) {
+    this.name = name;
+  }
+}
