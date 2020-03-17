cd72e9f4669dae799126f9ae2880dc2edfabbb92
LautaroBG <48892699+LautaroBG@users.noreply.github.com>
2020-03-17 19:50:54 -0300

    Exercise 1 W.I.P

---
+++ 04-HTML5-APIs/Exercise 1/scripts.js
@@ -0,0 +1,54 @@
+//localStorage
+function saveLocal(key, value){
+  localStorage.setItem(key, value);
+}
+
+function removeLocal(key){
+  localStorage.removeItem(key);
+}
+//indexedDB
+let openRequest = indexedDB.open("dataBase", 1);
+let db;
+
+openRequest.onerror = function(event){
+  alert("Database error: " + event.target.errorCode)
+};
+
+openRequest.onsuccess = function(event){
+  db = event.result;
+};
+
+openRequest.onupgradeneeded = function(event){ 
+  var db = event.target.result;
+
+  // objeto puede contener cualquier tipo de valor. La clave es generada automáticamente, o se puede indicar un argumento de clave distinto si se quiere usar una clave específica.
+  db.createObjectStore("text-to-store");
+};
+
+function saveIndexedDB(key, value){
+  var transaction = db.transaction([key], "readwrite");
+  var target = transaction.objectStore(key)
+  target.put(value, key);
+}
+
+function removeIndexedDB(key, value){
+  var transaction = db.transaction([key], "readwrite");
+  var target = transaction.objectStore(key);
+  target.delete(value);
+}
+
+const textArea = document.getElementById("text-area");
+const storeButton = document.getElementById("store-button");
+const clearButton = document.getElementById("clear-button");
+
+function saveText(event){
+  event.preventDefault();
+  saveLocal("text-to-store", textArea.value);
+  saveIndexedDB("text-to-store", textArea.value);
+}
+
+function removeText(event){
+  event.preventDefault();
+  removeLocal("text-to-store");
+  saveIndexedDB("text-to-store", textArea.value);
+}
